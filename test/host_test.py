7c46211111566508962b5a7a27f186acfe1e6304
Francesco Balducci <balau@users.sourceforge.net>
2011-08-18 14:38:35 +0200

    moved JTAGduino functionality in python tests in separate file. Added license. Changed default JTAG pins on Arduino.

--- test/host_test.py
+++ test/host_test.py
@@ -6,84 +6,20 @@
 # This script has been tested with python version 2.6.7
 
 import sys
-import serial
-import time
-
-class jtagduino_cmd:
-    CMD_IF_VER = 0x1
-    CMD_FW_VER = 0x2
-    CMD_SET_SERIAL_SPEED = 0x3
-    CMD_SET_PIN = 0x10
-    CMD_CLEAR_PIN = 0x11
-    CMD_GET_PIN = 0x12
-    CMD_PULSE_HIGH = 0x13
-    CMD_PULSE_LOW = 0x14
-    CMD_ASSIGN_PIN = 0x15  
-    CMD_SET_JTAG_SPEED = 0x20
-    CMD_JTAG_CLOCK = 0x21
-    CMD_JTAG_SEQUENCE = 0x22
-
-class jtagduino_rsp:
-    RSP_OK = 0
-    RSP_ERROR_BAD_CMD = 1
-    RSP_ERROR_UNKNOWN = 2
-    RSP_ERROR_BAD_PIN = 3
-    RSP_ERROR_BAD_SPEED = 4
-    RSP_BAD_SEQUENCE_LEN = 5
-    RSP_BAD_BAUD = 6
-
-class jtag_pins:
-    TCK = 0
-    TMS = 1
-    TDI = 2
-    TDO = 3
-    TRST = 4
-    N_JTAG_PINS = 5
+from jtagduino import *
 
 jtagduino_port = '/dev/ttyACM0'
 if (len(sys.argv) > 1):
     jtagduino_port = sys.argv[1]
 
-ser = serial.Serial(
-        port = jtagduino_port,
-        #baudrate = 115200,
-        baudrate = 9600,
-        )
-time.sleep(1.5)
-ser.flushInput()
-ser.flushOutput()
+device = jtagduino(jtagduino_port)
+print 'if_ver_major = %d if_ver_minor = %d' % device.if_ver()
+print 'fw_ver_major = %d fw_ver_minor = %d' % device.fw_ver()
+print 'clear pin TDI rsp = %d' % device.clear_pin(jtag_pins.TDI)
+print 'set pin TDI rsp = %d' % device.set_pin(jtag_pins.TDI)
+print 'get pin TDO rsp = %d val = %d' % device.get_pin(jtag_pins.TDO)
 
-print 'IF_VER'
-ser.write(chr(jtagduino_cmd.CMD_IF_VER))
-rsp = ord(ser.read(1))
-print 'rsp = %d' % rsp
-if (rsp == jtagduino_rsp.RSP_OK):
-    if_ver_minor = ord(ser.read(1))
-    if_ver_minor = if_ver_minor + (ord(ser.read(1)) * 256)
-    if_ver_major = ord(ser.read(1))
-    if_ver_major = if_ver_major + (ord(ser.read(1)) * 256)
-    print 'IF_VER_MINOR = %d' % if_ver_minor
-    print 'IF_VER_MAJOR = %d' % if_ver_major
-    print 'IF_VER OK'
-else:
-    print 'IF_VER KO'
-print
-
-print 'FW_VER'
-ser.write(chr(jtagduino_cmd.CMD_FW_VER))
-rsp = ord(ser.read(1))
-print 'rsp = %d' % rsp
-if (rsp == jtagduino_rsp.RSP_OK):
-    fw_ver_minor = ord(ser.read(1))
-    fw_ver_minor = fw_ver_minor + (ord(ser.read(1)) * 256)
-    fw_ver_major = ord(ser.read(1))
-    fw_ver_major = fw_ver_major + (ord(ser.read(1)) * 256)
-    print 'FW_VER_MINOR = %d' % fw_ver_minor
-    print 'FW_VER_MAJOR = %d' % fw_ver_major
-    print 'FW_VER OK'
-else:
-    print 'FW_VER KO'
-print
+sys.exit(0);
 
 #TODO: other commands
 
---
+++ test/jtagduino.py
@@ -0,0 +1,89 @@
+
+import serial
+import time
+
+class jtagduino_cmd:
+    CMD_IF_VER = 0x1
+    CMD_FW_VER = 0x2
+    CMD_SET_SERIAL_SPEED = 0x3
+    CMD_SET_PIN = 0x10
+    CMD_CLEAR_PIN = 0x11
+    CMD_GET_PIN = 0x12
+    CMD_PULSE_HIGH = 0x13
+    CMD_PULSE_LOW = 0x14
+    CMD_ASSIGN_PIN = 0x15  
+    CMD_SET_JTAG_SPEED = 0x20
+    CMD_JTAG_CLOCK = 0x21
+    CMD_JTAG_SEQUENCE = 0x22
+
+class jtagduino_rsp:
+    RSP_OK = 0
+    RSP_ERROR_BAD_CMD = 1
+    RSP_ERROR_UNKNOWN = 2
+    RSP_ERROR_BAD_PIN = 3
+    RSP_ERROR_BAD_SPEED = 4
+    RSP_BAD_SEQUENCE_LEN = 5
+    RSP_BAD_BAUD = 6
+
+class jtag_pins:
+    TCK = 0
+    TMS = 1
+    TDI = 2
+    TDO = 3
+    TRST = 4
+    N_JTAG_PINS = 5
+
+class jtagduino:
+    def __init__(self, port='/dev/ttyACM0'):
+        self.ser = serial.Serial(
+            port = port,
+            #baudrate = 115200,
+            baudrate = 9600,
+        )
+        time.sleep(1.5)
+        self.ser.flushInput()
+        self.ser.flushOutput()
+    def if_ver(self):
+        self.ser.write(chr(jtagduino_cmd.CMD_IF_VER))
+        rsp = ord(self.ser.read(1))
+        if (rsp == jtagduino_rsp.RSP_OK):
+            if_ver_minor = ord(self.ser.read(1))
+            if_ver_minor = if_ver_minor + (ord(self.ser.read(1)) * 256)
+            if_ver_major = ord(self.ser.read(1))
+            if_ver_major = if_ver_major + (ord(self.ser.read(1)) * 256)
+            return (if_ver_major, if_ver_minor)
+        else:
+            return None
+    def fw_ver(self):
+        self.ser.write(chr(jtagduino_cmd.CMD_FW_VER))
+        rsp = ord(self.ser.read(1))
+        if (rsp == jtagduino_rsp.RSP_OK):
+            fw_ver_minor = ord(self.ser.read(1))
+            fw_ver_minor = fw_ver_minor + (ord(self.ser.read(1)) * 256)
+            fw_ver_major = ord(self.ser.read(1))
+            fw_ver_major = fw_ver_major + (ord(self.ser.read(1)) * 256)
+            return (fw_ver_major, fw_ver_minor)
+        else:
+            return None
+    def set_pin(self, pin):
+        self.ser.write(chr(jtagduino_cmd.CMD_SET_PIN))
+        self.ser.write(chr(pin))
+        rsp = ord(self.ser.read(1))
+        return rsp;
+    def clear_pin(self, pin):
+        self.ser.write(chr(jtagduino_cmd.CMD_CLEAR_PIN))
+        self.ser.write(chr(pin))
+        rsp = ord(self.ser.read(1))
+        return rsp;
+    def get_pin(self, pin):
+        self.ser.write(chr(jtagduino_cmd.CMD_GET_PIN))
+        self.ser.write(chr(pin))
+        rsp = ord(self.ser.read(1))
+        if (rsp == jtagduino_rsp.RSP_OK):
+            val = ord(self.ser.read(1))
+        else:
+            val = 0
+        return (rsp, val)
+
+        
+
